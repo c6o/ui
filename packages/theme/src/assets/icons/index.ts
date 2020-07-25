d8df9b0bd9e0337cc820b5fab9a368ccdb6f53e8
Adrian Jones <adrian@codezero.io>
2020-07-25 12:33:41 -0700

    feat(icons): Moved icon assets to ui-theme package

---
+++ packages/theme/src/assets/icons/index.ts
@@ -0,0 +1,34 @@
+// Blue Icons
+import appStoreIconB from './blue/app-store.svg'
+import cloudIconB from './blue/cloud.svg'
+import installIconB from './blue/install.svg'
+import revenueIconB from './blue/revenue.svg'
+
+export const appStoreIconBlue = appStoreIconB
+export const cloudIconBlue = cloudIconB
+export const installIconBlue = installIconB
+export const revenueIconBlue = revenueIconB
+
+// Green icons
+import marketplaceIconG from './green/marketplace.svg'
+import toolsIconG from './green/tools.svg'
+
+export const marketplaceIconGreen = marketplaceIconG
+export const toolsIconGreen = toolsIconG
+
+// Grey Icons
+import installationsIconGr from './grey/installations.svg'
+
+export const installationsIconGrey = installationsIconGr
+
+// Red Icons
+import userIconR from './red/user.svg'
+
+export const userIconRed = userIconR
+
+// Yellow Icons
+import cloudIconY from './yellow/cloud.svg'
+import warningIconY from './yellow/warning.svg'
+
+export const cloudIconYellow = cloudIconY
+export const warningIconYellow = warningIconY
