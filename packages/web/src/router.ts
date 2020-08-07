66bc4b5acb754801b725100065b1212a69273343
Adrian Jones <ajones@popdigital.ca>
2020-08-06 18:22:21 -0700

    fix navigate function

--- packages/web/src/router.ts
+++ packages/web/src/router.ts
@@ -5,8 +5,8 @@ export function navigate(e, path = '', param = 'value') {
     e.preventDefault()
     if (e.target.value)
         Router.go(`/${path}?${param}=${e.target.value}`)
-    else if (e.currentTarget.id)
-        Router.go(`/${path}?${param}=${e.currentTarget.id}`)
+    else if (e.target.dataset.value)
+        Router.go(`/${path}?${param}=${e.target.dataset.value}`)
     else
         Router.go(`/${path}`)
 }
