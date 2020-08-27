a622d86b841c2425eb26ede6d5a16323af7695d6
Adrian Jones <ajones@popdigital.ca>
2020-08-27 11:47:44 -0700

    chore: Simplify the router util methods; remove 'redirect' and just use 'navigate'

--- packages/web/src/router.ts
+++ packages/web/src/router.ts
@@ -1,16 +1,13 @@
 // Router utility functions
 import { Router } from '@vaadin/router'
 
-export function navigate(e, path = '', param = 'value') {
-    e.preventDefault()
-    if (e.target.value)
-        Router.go(`/${path}?${param}=${e.target.value}`)
-    else if (e.target.dataset.value)
-        Router.go(`/${path}?${param}=${e.target.dataset.value}`)
-    else
+export function navigate(path = '', e = null, param = 'value') {
+    if (e) {
+        e.preventDefault()
+        if (e.target.value)
+            Router.go(`/${path}?${param}=${e.target.value}`)
+        else if (e.target.dataset.value)
+            Router.go(`/${path}?${param}=${e.target.dataset.value}`)
+    } else
         Router.go(`/${path}`)
-}
-
-export function redirect(path = '') {
-    Router.go(`/${path}`)
 }
