ebc5900a6a80cdab5ae559f6360efcef03e6447d
Adrian Jones <ajones@popdigital.ca>
2020-09-04 12:34:22 -0700

    overlay to the loader

--- packages/web/src/loaders/loader.ts
+++ packages/web/src/loaders/loader.ts
@@ -1,4 +1,4 @@
-import { html, customElement, property, CSSResult } from 'lit-element'
+import { html, customElement, property, css, CSSResult } from 'lit-element'
 import { MobxLitElement } from '@adobe/lit-mobx'
 import { EntityStore, EntityListStore } from '@c6o/common'
 import { cssReboot, cssBase, cssGrid } from '@c6o/ui-theme'
@@ -22,14 +22,56 @@ export class Loader extends MobxLitElement {
         return [
             cssReboot,
             cssBase,
-            cssGrid
+            cssGrid,
+            css`
+                #content {
+                    position: relative;
+                }
+
+                #overlay {
+                    background-color: var(--color-snow);
+                    display: none;
+                    height: 100%;
+                    opacity: .5;
+                    position: absolute;
+                    width: 100%;
+                    z-index: 100;
+                }
+
+                #overlay.busy {
+                    display: block;
+                }
+
+                @keyframes spinner {
+                    to { transform: rotate(360deg); }
+                }
+
+                #overlay.busy:before {
+                    content: '';
+                    box-sizing: border-box;
+                    position: fixed;
+                    top: 50%;
+                    left: 50%;
+                    width: 40px;
+                    height: 40px;
+                    margin-top: -20px;
+                    margin-left: -20px;
+                    border-radius: 50%;
+                    border-top: 2px solid var(--color-twilight-ocean);
+                    border-right: 2px solid transparent;
+                    animation: spinner .4s linear infinite;
+                }
+            `
         ]
     }
 
     render() {
         if (this.store?.initialized) {
             return this.store.success ? html`
-                <slot></slot>
+                <section id="content">
+                    <div id="overlay" class="${this.store.busy ? 'busy' : ''}"></div>
+                    <slot></slot>
+                </section>
             ` : html`
                 <c6o-errors heading="The system encountered an error:" .store=${this.store}></c6o-errors>
             `
