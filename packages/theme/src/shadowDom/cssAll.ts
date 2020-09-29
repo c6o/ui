ebe8f679d6fceb7f3a063db7b51a22529c0cf606
Adrian Jones <ajones@popdigital.ca>
2020-09-28 18:20:25 -0700

    styling for the c6o-dialog component

--- packages/theme/src/shadowDom/cssAll.ts
+++ packages/theme/src/shadowDom/cssAll.ts
@@ -6,7 +6,7 @@ import { cssLayouts } from './cssLayouts'
 import { cssReboot } from './cssReboot'
 import { cssTypography } from './cssTypography'
 
-// Does not include cssTooltip or cssBadge
+// Does not include cssTooltip, cssBadge, cssModals
 export const cssAll = [
     cssReboot,
     cssGrid,
