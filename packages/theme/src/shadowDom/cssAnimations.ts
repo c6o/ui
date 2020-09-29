eceff6d73745825cf06e900cdf1b06b5ff8a46bd
Adrian Jones <ajones@popdigital.ca>
2020-09-29 14:51:55 -0700

    chore: Move the installing and error animations to ui-theme

--- packages/theme/src/shadowDom/cssAnimations.ts
+++ packages/theme/src/shadowDom/cssAnimations.ts
@@ -80,4 +80,54 @@ export const cssAnimations = css`
         border-right: 2px solid transparent;
         animation: spinner .4s linear infinite;
     }
+
+    .icon.installing {
+        -webkit-animation: sail 2.2s infinite ease-in-out;
+        animation: sail 2.2s infinite ease-in-out;
+    }
+
+    .icon.error {
+        -webkit-animation: pulse 1.2s infinite ease-in-out;
+        animation: pulse 1.2s infinite ease-in-out;
+    }
+
+    @keyframes sail {
+        0% {
+            transform: translatey(0px) rotate(-5deg);
+            -webkit-transform: translatey(0px) rotate(-5deg);
+        }
+        25% {
+            transform: translatey(-6px);
+            -webkit-transform: transform: translatey(-6px);
+        }
+        50% {
+            transform: translatey(0px) rotate(5deg);
+            -webkit-transform: translatey(0px) rotate(5deg);
+        }
+        75% {
+            transform: translatey(0px);
+            -webkit-transform: translatey(0px)
+        }
+        100% {
+            transform: translatey(0px) rotate(-5deg);
+            -webkit-transform: translatey(0px) rotate(-5deg);
+        }
+    }
+
+    @keyframes pulse {
+        0% {
+            transform: scale(1);
+            filter: drop-shadow(0 0 0 var(--color-night-fire));
+        }
+
+        70% {
+            transform: scale(1.05);
+            filter: drop-shadow(3px 3px 3px var(--color-night-fire));
+        }
+
+        100% {
+            transform: scale(1);
+            filter: drop-shadow(0 0 0 var(--color-night-fire));
+        }
+    }
 `
