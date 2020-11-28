661360e29d955ac2d8468514efbf2950ac74d659
Adrian Jones <ajones@popdigital.ca>
2020-11-27 16:15:44 -0800

    chore: Allow the InfoDialog header and footer to be customized

--- packages/web/src/dialogs/infoDialog.ts
+++ packages/web/src/dialogs/infoDialog.ts
@@ -1,9 +1,12 @@
-import { html, customElement, property, CSSResult } from 'lit-element'
+import { html, customElement, property, css, CSSResult } from 'lit-element'
 import { cssAll, cssModals } from '@c6o/ui-theme'
 import { BaseDialog } from './baseDialog'
 
 @customElement('c6o-info-dialog')
 export class InfoDialog extends BaseDialog {
+
+    footer?()
+    header?()
 
     @property({ type: Boolean, attribute: 'has-previous' })
     hasPrevious = false
@@ -11,31 +14,46 @@ export class InfoDialog extends BaseDialog {
     static get styles(): (CSSResult[] | CSSResult)[] {
         return [
             cssAll,
-            cssModals
+            cssModals,
+            css`
+                .animation-container {
+                    position: relative;
+                }
+
+                .waiting-animation {
+                    left: -45px;
+                    position: absolute;
+                    top: 3px;
+                }
+            `
         ]
     }
 
     render() {
         return html`
             <c6o-dialog classes=${this.cssClasses} ?opened=${this.opened}>
-                <header slot="header">
-                    <h2 class="modal-title">
-                        ${this.title}
-                        ${this.subtitle ? html`
-                            <div class="modal-subtitle">${this.subtitle}</div>
-                        `: ''}
-                    </h2>
-                    <iron-icon icon="vaadin:close" @click=${this.close}></iron-icon>
-                </header>
+                ${this.header ? this.header() : html`
+                    <header slot="header">
+                        <h2 class="modal-title">
+                            ${this.title}
+                            ${this.subtitle ? html`
+                                <div class="modal-subtitle">${this.subtitle}</div>
+                            `: ''}
+                        </h2>
+                        <iron-icon icon="vaadin:close" @click=${this.close}></iron-icon>
+                    </header>
+                `}
 
                 <slot></slot>
 
-                <footer c6o="${this.hasPrevious ? 'flex justify-between' : 'text-right'}" slot="footer">
-                    ${this.hasPrevious ? html`
-                        <c6o-button theme="${this.btnTheme}" @click=${this.previous}>Previous</c6o-button>
-                    ` : ''}
-                    <c6o-button class="close-button" theme="${this.btnTheme}" @click=${this.close}>${this.btnText}</c6o-button>
-                </footer>
+                ${this.footer ? this.footer() : html`
+                    <footer c6o="${this.hasPrevious ? 'flex justify-between' : 'text-right'}" slot="footer">
+                        ${this.hasPrevious ? html`
+                            <c6o-button theme="${this.btnTheme}" @click=${this.previous}>Previous</c6o-button>
+                        ` : ''}
+                        <c6o-button class="close-button" theme="${this.btnTheme}" @click=${this.close}>${this.btnText}</c6o-button>
+                    </footer>
+                `}
             </c6o-dialog>
         `
     }
