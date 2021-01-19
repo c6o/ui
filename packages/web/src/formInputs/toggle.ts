b0ca551a871122d414282df98a360bbf776419e5
Adrian Jones <adrian@codezero.io>
2021-01-19 13:51:15 -0800

    fix: Toggle component refactor

--- packages/web/src/formInputs/toggle.ts
+++ packages/web/src/formInputs/toggle.ts
@@ -1,8 +1,5 @@
-import { html, customElement, css, property, CSSResult } from 'lit-element'
+import { LitElement, html, customElement, css, property, CSSResult } from 'lit-element'
 import '@polymer/paper-toggle-button/paper-toggle-button.js'
-import { MobxLitElement } from '@adobe/lit-mobx'
-import { mix } from 'mixwith'
-import { EntityStoreMixin } from '../mixins'
 import { cssReboot, cssBase, cssGrid, cssTypography } from '@c6o/ui-theme'
 
 /**
@@ -21,29 +18,24 @@ import { cssReboot, cssBase, cssGrid, cssTypography } from '@c6o/ui-theme'
  *
  *  Attribute | Description
  * ------------|------------
- * `checked`   | Whether the toggle is on (checked) or off
+ * `is-checked` | Whether the toggle is on (checked) or off
  * `label-off` | The left-hand label for the OFF position
  * `label-on`  | The right-hand label for the ON position
  *
- * @extends MobxLitElement
- * @mixes MobxLitElement
- * @mixes EntityStoreMixin
+ * @extends LitElement
  */
 
-export interface Toggle extends EntityStoreMixin {
-}
-
 @customElement('c6o-toggle')
-export class Toggle extends mix(MobxLitElement).with(EntityStoreMixin) {
+export class Toggle extends LitElement {
 
-    @property({ type: Boolean })
-    checked: boolean
+    @property({ type: Boolean, attribute: 'is-checked' })
+    isChecked: boolean
 
     @property({ type: String, attribute: 'label-off' })
-    labelOff: string
+    labelOff = 'OFF'
 
     @property({ type: String, attribute: 'label-on' })
-    labelOn: string
+    labelOn = 'ON'
 
     static get styles(): (CSSResult[] | CSSResult)[] {
         return [
@@ -91,26 +83,26 @@ export class Toggle extends mix(MobxLitElement).with(EntityStoreMixin) {
         return html`
             <c6o-flex v-centered>
                 ${this.labelOff ? html`
-                    <label class="off inline ${this.checked ? '' : 'active'}">${this.labelOff}</label>
+                    <label class="off inline ${this.isChecked ? '' : 'active'}">${this.labelOff}</label>
                 ` : ''}
 
-                <paper-toggle-button c6o="fit" ?checked=${this.checked} @click="${this.handleToggleClick}">
+                <paper-toggle-button c6o="fit" ?checked=${this.isChecked} @click="${this.handleToggleClick}">
                     ${!this.labelOff && !this.labelOn ? html`
                         <span id="label">
                             <slot></slot>
                         </span>
                     ` : ''}
-
                 </paper-toggle-button>
+
                 ${this.labelOn ? html`
-                    <label class="on inline ${this.checked ? 'active' : ''}">${this.labelOn}</label>
+                    <label class="on inline ${this.isChecked ? 'active' : ''}">${this.labelOn}</label>
                 ` : ''}
             </c6o-flex>
         `
     }
 
     handleToggleClick = (e) => {
-        this.checked = e.target.checked
+        this.isChecked = e.target.checked
         const customEvent = new CustomEvent('toggle', {
             detail: e.target.checked,
             bubbles: true,
